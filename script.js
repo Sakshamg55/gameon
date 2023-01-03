dbc0e8d936a1a171728c32120034c627a0790171
Saksham Gupta <121820968+Sakshamg55@users.noreply.github.com>
2023-01-03 10:53:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.onkeydown = function(e) {
+    console.log("Key code is: ", e.key Code)
+    if(e.key code==){
+      dino = document.querySelector('.dino');
+      dino.classList.add('animateDino');
+      setTimeout(() => {
+          console.log("yes")
+       dino.classList.remove('animateDino')
+      }, );
+    
+}
